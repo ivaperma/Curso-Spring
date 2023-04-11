9450b76424640a2712e10c1c0f4d53f13565e1d5
ivaperma <ivanpm89@gmail.com>
2023-04-11 17:20:23 +0200

    starting with JWT

---
+++ spring-boot-jwt/src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.bolsadeideas.springboot.app.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.collections4.map.HashedMap;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/api/login", "POST"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = this.obtainUsername(request);
+        username = username != null ? username.trim() : "";
+        String password = this.obtainPassword(request);
+        password = password != null ? password : "";
+        if (username != null && password != null) {
+            logger.info("Username desde resquest parameter (form-data): " + username);
+            logger.info("Password desde resquest parameter (form-data): " + password);
+        }
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        String username = ((User) authResult.getPrincipal()).getUsername();
+        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        String token = Jwts.builder()
+                .setSubject(username)
+                .signWith(secretKey)
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
+        Map<String, Object> body = new HashMap<String, Object>();
+        body.put("token", token);
+        body.put("user", (User) authResult.getPrincipal());
+        body.put("mensaje", String.format("Hola %s, has iniciado sesión con éxito", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(200);
+        response.setContentType("application/json");
+    }
+}
