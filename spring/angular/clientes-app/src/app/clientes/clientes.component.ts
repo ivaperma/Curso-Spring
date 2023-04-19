cffaaf7b4b69562b4fbef0db5c9c63d3d614513b
ivaperma <ivanpm89@gmail.com>
2023-04-19 14:20:29 +0200

    implementing Angular services and routes

---
+++ spring/angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html'
+})
+export class ClientesComponent {
+
+  clientes!: Cliente[];
+
+  constructor(private ClienteService: ClienteService){
+  }
+
+  ngOnInit(): void {
+    this.ClienteService.getClientes().subscribe(
+      clientes => this.clientes = clientes
+    );
+  }
+
+}
---
+++ spring/angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Iván', apellido: 'Pérez', email: 'ivanpm89@gmail.com', createAt: '2023-04-19'},
+    {id: 2, nombre: 'Peter', apellido: 'Pan', email: 'peter@gmail.com', createAt: '2023-04-19'},
+    {id: 3, nombre: 'Jaime', apellido: 'Gómez', email: 'jaime@gmail.com', createAt: '2023-04-19'},
+    {id: 4, nombre: 'Silvia', apellido: 'Mata', email: 'silvia@gmail.com', createAt: '2023-04-19'},
+    {id: 5, nombre: 'María', apellido: 'Sánz', email: 'maria@gmail.com', createAt: '2023-04-19'},
+    {id: 6, nombre: 'Ralph', apellido: 'Lauren', email: 'ralph@gmail.com', createAt: '2023-04-19'},
+    {id: 7, nombre: 'Tom', apellido: 'Hardy', email: 'toom@gmail.com', createAt: '2023-04-19'},
+  ];
