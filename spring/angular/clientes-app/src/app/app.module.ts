4b391ca94b7e304bbc317f4d5e22070c6d660aa6
ivaperma <ivanpm89@gmail.com>
2023-04-23 21:16:05 +0200

    connected Angular into backend with CORS

--- spring/angular/clientes-app/src/app/app.module.ts
+++ spring/angular/clientes-app/src/app/app.module.ts
@@ -7,7 +7,8 @@ import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
+ 
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectivaComponent},
@@ -24,6 +25,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- spring/angular/clientes-app/src/app/clientes/cliente.service.ts
+++ spring/angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -3,15 +3,23 @@ import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe(
+      map((response) => response as Cliente[])
+    );
   }
+
 }
